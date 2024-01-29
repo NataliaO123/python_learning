b7fdaf5d21c7974769067bcd3c38d48aa92ac441
NataliaO123 <osipovan965@gmail.com>
2024-01-29 11:13:58 +0200

    some string/list method repetition

--- learn_python.py
+++ learn_python.py
@@ -2736,3 +2736,9 @@ s = input().split()
 for i in range(0, len(s) - 1, 2):
     s[i], s[i + 1] = s[i + 1], s[i]
 print(*s)
+
+## Сдвиг в развитии
+
+n = input().split()
+last = n.pop()
+print(last, *n)

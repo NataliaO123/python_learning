b87fe07fe870efc2b0b9faafec3f83fc720de822
NataliaO123 <osipovan965@gmail.com>
2024-02-04 11:02:49 +0200

    nested lists

--- learn_python.py
+++ learn_python.py
@@ -2851,3 +2851,10 @@ for i in range(n):
             s.pop(0)
     if not seq:
         print(i + 1, end=" ")
+
+## Вложенные списки
+
+list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
+list1[2][2].append(7000)
+
+print(list1)

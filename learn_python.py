8ec2a82e3f6f54025f5211e2bf920efa5cbc5ba8
NataliaO123 <osipovan965@gmail.com>
2024-02-05 10:51:38 +0200

    new tasks with nested lists

--- learn_python.py
+++ learn_python.py
@@ -2858,3 +2858,37 @@ list1 = [10, 20, [300, 400, [5000, 6000], 500], 30, 40]
 list1[2][2].append(7000)
 
 print(list1)
+
+## 
+
+list1 = ['a', 'b', ['c', ['d', 'e', ['f', 'g'], 'k'], 'l'], 'm', 'n']
+sub_list = ['h', 'i', 'j']
+list1[2][1][2].extend(sub_list)
+print(list1)
+
+##
+
+list1 = [[1, 7, 8], [9, 7, 102], [6, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+maximum = 0
+for i in range(len(list1)):
+    for j in list1[i]:
+        if j > maximum:
+            maximum = j
+print(maximum)
+
+##
+
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+for i in range(len(list1)):
+    list1[i].reverse()
+print(list1)
+
+##
+
+list1 = [[1, 7, 8], [9, 7, 102], [102, 106, 105], [100, 99, 98, 103], [1, 2, 3]]
+total = 0
+counter = 0
+for i in range(len(list1)):
+    counter += len(list1[i])
+    total += sum(list1[i])
+print(total/counter)

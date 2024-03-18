02592dd5953c218ac9ebb7e2c9b01e9cbc6750f0
NataliaO123 <osipovan965@gmail.com>
2024-03-18 12:04:28 +0200

    matrix and spiral

--- learn_python.py
+++ learn_python.py
@@ -3672,3 +3672,48 @@ for i in range(n + m):
             if j + k == i:
                 matrix[j][k] = temp
                 temp += 1   
+
+## Заполнение спиралью
+
+def spiral(rows, cols):
+    matrix = [[0] * cols for _ in range(rows)]
+    num = 1
+    top_row = 0
+    bottom_row = rows - 1
+    left_col = 0
+    right_col = cols - 1
+
+    while top_row <= bottom_row and left_col <= right_col:
+
+        for i in range(left_col, right_col + 1):
+            matrix[top_row][i] = num
+            num +=1
+        top_row += 1
+
+        for i in range(top_row, bottom_row + 1):
+            matrix[i][right_col] = num
+            num += 1
+        right_col -= 1
+
+        if top_row <= bottom_row:
+            for i in range(right_col, left_col - 1, -1):
+                matrix[bottom_row][i] = num
+                num += 1
+            bottom_row -= 1
+
+        if left_col <= right_col:
+            for i in range(bottom_row, top_row - 1, -1):
+                matrix[i][left_col] = num
+                num += 1
+            left_col += 1
+
+    for i in matrix:
+        print(*i)
+    print()
+        
+
+params = input().split()
+n = int(params[0])
+m = int(params[1])
+spiral(n, m)
+

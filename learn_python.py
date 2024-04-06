85b2b6c435fe85763107b33ed11b1fe0d09afb6b
NataliaO123 <osipovan965@gmail.com>
2024-04-06 09:07:11 +0300

    sets

--- learn_python.py
+++ learn_python.py
@@ -4167,3 +4167,32 @@ set3 = set(int(i) for i in input().split())
 resp = (set1 | set2 | set3) - (set1 & set2 & set3)
 
 print(*sorted(resp))
+
+## Урок биологии
+
+set1 = set(int(i) for i in input().split())
+set2 = set(int(i) for i in input().split())
+set3 = set(int(i) for i in input().split())
+
+all = {int(i) for i in range(11)}
+resp = all - (set1 | set2 | set3)
+
+print(*sorted(resp))
+
+## Урок физики
+
+set1 = set(int(i) for i in input().split())
+set2 = set(int(i) for i in input().split())
+set3 = set(int(i) for i in input().split())
+
+resp = set3 - (set1 | set2)
+
+print(*sorted(resp, reverse = True))
+
+## 
+
+sentence = '''My very photogenic mother died in a freak accident (picnic, lightning) when I was three, and, save for a pocket of warmth in the darkest past, nothing of her subsists within the hollows and dells of memory, over which, if you can still stand my style (I am writing under observation), the sun of my infancy had set: surely, you all know those redolent remnants of day suspended, with the midges, about some hedge in bloom or suddenly entered and traversed by the rambler, at the bottom of a hill, in the summer dusk; a furry warmth, golden midges.'''
+myset = {word.lower().strip('.,;:-?!()') for word in sentence.split() if len(word) < 4}
+print(*sorted(myset))
+
+

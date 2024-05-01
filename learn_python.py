424199e771f0b4a520befa4516f67414be60acea
NataliaO123 <osipovan965@gmail.com>
2024-05-01 08:58:35 +0300

    random in python

--- learn_python.py
+++ learn_python.py
@@ -5041,3 +5041,173 @@ def generate_passwords(count, length):
 
 n, m = int(input()), int(input())
 generate_passwords(n, m)
+
+## Тайный друг
+
+import random
+
+students = []
+for i in range(int(input())):
+    students.append(input())
+
+for i in range(len(students)):
+    print(students[i], '-', students[i - 1])
+
+## Генератор паролей 2
+
+import random
+import string
+
+def generate_password(length):
+    f = False
+    chars = string.ascii_letters + string.digits
+    restricted = {'l', 'I', '1', 'o', 'O', '0'}
+    available_chars = ''.join(c for c in chars if c not in restricted)
+
+    upper = set(string.ascii_uppercase)
+    lower = set(string.ascii_lowercase)
+    digit = set(string.digits)
+
+    while not f:
+        temp_list = list(available_chars)
+        random.shuffle(temp_list)
+        pwd = ''.join(temp_list[:length])
+        temp = set(pwd)
+        if len(temp & upper) > 0 and len(temp & lower)  > 0 and len(temp & digit) > 0:
+            f = True
+            
+    return pwd
+
+def generate_passwords(count, length):
+    for _ in range(count):
+        print(generate_password(length))
+
+n, m = int(input()), int(input())
+generate_passwords(n, m)
+
+## Monte Carlo Simulation
+
+import random
+
+n = 10**6
+s0 = 16
+k = 0
+
+for _ in range(n):
+    x = random.uniform(-2, 2)
+    y = random.uniform(-2, 2)
+
+    if -2 <= x <= 2 and -2 <= y <= 2 and (x**3 + y**4 + 2) >= 0 and (3 * x + y** 2) <= 2:
+        k += 1
+
+print((k / n) * s0)
+
+## Monte Carlo Simulation and Pi
+
+import random
+
+n = 10**6
+k = 0
+s0 = 4
+
+for _ in range(n):
+    x = random.uniform(-1, 1)
+    y = random.uniform(-1, 1)
+
+    if x**2 + y**2 <= 1:
+        k += 1
+
+print((k / n) * s0)
+
+## Decimal!
+
+from decimal import *
+
+s = '0.77 4.03 9.06 3.80 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.23 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50'
+
+temp = [Decimal(i) for i in s.split()]
+print(max(temp) + min(temp))
+
+##
+
+s = '9.73 8.84 8.92 9.60 9.32 8.97 8.53 1.26 6.62 9.85 1.85 1.80 0.83 6.75 9.74 9.11 9.14 5.03 5.03 1.34 3.52 8.09 7.89 8.24 8.23 5.22 0.30 2.59 1.25 6.24 2.14 7.54 5.72 2.75 2.32 2.69 9.32 8.11 4.53 0.80 0.08 9.36 5.22 4.08 3.86 5.56 1.43 8.36 6.29 5.13'
+
+from decimal import *
+temp = [Decimal(i) for i in s.split()]
+print(sum(temp))
+
+mx = []
+max_d = 0
+
+for i in range(5):
+    max_d = max(temp)
+    mx.append(max_d)
+    temp.remove(max_d)
+
+print(*sorted(mx, reverse = True))
+
+##
+
+from decimal import *
+num = abs(Decimal(input()))
+num_tuple = num.as_tuple().digits
+if num < 1:
+    print(max(num_tuple))
+else:
+    print(max(num_tuple) + min(num_tuple))
+
+##
+
+from decimal import *
+
+d = Decimal(input())
+
+print(d.exp() + d.ln() + d.log10() + d.sqrt())
+
+from fractions import Fraction
+
+numbers = ['6.34', '4.08', '3.04', '7.49', '4.45', '5.39', '7.82', '2.76', '0.71', '1.97', '2.54', '3.67', '0.14', '4.29', '1.84', '4.07', '7.26', '9.37', '8.11', '4.30', '7.16', '2.46', '1.27', '0.29', '5.12', '4.02', '6.95', '1.62', '2.26', '0.45', '6.91', '7.39', '0.52', '1.88', '8.38', '0.75', '0.32', '4.81', '3.31', '4.63', '7.84', '2.25', '1.10', '3.35', '2.05', '7.87', '2.40', '1.20', '2.58', '2.46']
+
+resp = [Fraction(i) for i in numbers]
+
+for i in range(len(resp)):
+    print(numbers[i], '=', resp[i])
+
+##
+
+from fractions import Fraction
+
+s = '0.78 4.3 9.6 3.88 7.08 5.88 0.23 4.65 2.79 0.90 4.23 2.15 3.24 8.57 0.10 8.57 1.49 5.64 3.63 8.36 1.56 6.67 1.46 5.26 4.83 7.13 1.22 1.02 7.82 9.97 5.40 9.79 9.82 2.78 2.96 0.07 1.72 7.24 7.84 9.23 1.71 6.24 5.78 5.37 0.03 9.60 8.86 2.73 5.83 6.50 0.123 0.00021'
+
+temp = [Fraction(i) for i in s.split()]
+print(Fraction(max(temp)) + Fraction(min(temp)))
+
+##
+
+from fractions import Fraction
+
+m, n = int(input()), int(input())
+print(Fraction(m, n))
+
+##
+
+from fractions import Fraction
+a, b = input(), input()
+a1, b1 = Fraction(a), Fraction(b)
+
+print(a, '+', b, '=', a1 + b1)
+print(a, '-', b, '=', a1 - b1)
+print(a, '*', b, '=', a1 * b1)
+print(a, '/', b, '=', a1 / b1)
+
+## Сумма дробей 1
+
+from fractions import Fraction
+
+n = int(input())
+s = 0
+
+for i in range(n):
+    s += 1 / (i**2)
+
+print(Fraction(s))

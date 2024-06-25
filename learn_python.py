6153a1e1edda7e1a6d551fb365a80c33b39989f8
NataliaO123 <osipovan965@gmail.com>
2024-06-25 14:27:14 +0300

    ^_^

--- learn_python.py
+++ learn_python.py
@@ -5698,3 +5698,12 @@ def ignore_command(command):
 def ignore_command(command):
     ignore = ['alias', 'configuration', 'ip', 'sql', 'select', 'update', 'exec', 'del', 'truncate']
     return any(word in command for word in ignore)
+
+##
+
+countries = ['Russia', 'USA', 'UK', 'Germany', 'France', 'India']
+capitals = ['Moscow', 'Washington', 'London', 'Berlin', 'Paris', 'Delhi']
+population = [145_934_462, 331_002_651, 80_345_321, 67_886_011, 65_273_511, 1_380_004_385]
+
+for i, j, k in zip(capitals, countries, population):
+    print(f"{i} is the capital of {j}, population equal {k} people.")
